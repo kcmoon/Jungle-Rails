fad63b4c500477aafd2ab575a28e120f5df34099
Kenny Moon <kennymoon90@gmail.com>
2022-07-27 15:33:42 -0700

    Fetch data from database tables

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,9 @@ class Admin::DashboardController < ApplicationController
 
   http_basic_authenticate_with name: ENV["HTTP_AUTH_USER"], password: ENV["HTTP_AUTH_PASS"]
   
+  # Access date from Category and Product tables
   def show
+    @category = Category.all.count
+    @products = Product.all.count
   end
 end
